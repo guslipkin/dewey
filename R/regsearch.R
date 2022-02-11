fd19e9a7a0608ae1ec2260da4febee4fa29845d9
guslipkin <gus.lipkin@gmail.com>
2022-02-11 14:16:10 -0500

    Big updates for readability and usability

--- R/regsearch.R
+++ R/regsearch.R
@@ -25,6 +25,8 @@
 #' @param multi (Optional) A boolean indicating whether or not multithreading
 #'   should be used. Defaults to `FALSE`. It is highly recommended to use
 #'   multithreading.
+#' @param ... (Optional) Function arguments to be passed to
+#'   \code{\link[stats]{glm}}
 #'
 #' @return Returns a `data.table` of information on the regressions run. The
 #'   resulting data.table is sorted in descending order by the rSquare divided
@@ -64,7 +66,8 @@ regsearch <- function(data,
                       family,
                       topN = 0,
                       interactions = FALSE,
-                      multi = FALSE) {
+                      multi = FALSE,
+                      ...) {
 
   # warnings for missing variables in function call
   if(missing(interactions)) {
@@ -87,7 +90,8 @@ regsearch <- function(data,
          call. = FALSE)
   }
 
-  maxvarTest <- length(independent) + ifelse(interactions, choose(length(independent), 2), 0)
+  maxvarTest <-
+    length(independent) + ifelse(interactions, choose(length(independent), 2), 0)
   if(maxvar > maxvarTest) {
     warning(paste("`maxvar` is greater than the number of independent variables. Setting maxvar to",
                   maxvarTest, collapse = ""))
@@ -100,7 +104,8 @@ regsearch <- function(data,
   # Get every combination of the independent variables
   # append underscores to factor variables so that when the regression is run,
   #   they will show as `factor_1`, `factor_2`, and so on
-  varNames <- ifelse(sapply(data, is.factor), paste0(names(data), "_"), names(data))
+  varNames <-
+    ifelse(sapply(data, is.factor), paste0(names(data), "_"), names(data))
   # create and sort the list of independent variables
   if(interactions) {
     independent <- sort(independent)
@@ -129,42 +134,18 @@ regsearch <- function(data,
   #   i.e. `a*b + a + b` is dropped in favor of `a*b` since `+ a + b` is implied
   print("Assembling regresions...")
   if(multi) {
-    combs <- pblapply(cl = clust, minvar:maxvar,
-                      function(x) {
-                        y <- combn(independent, x, simplify = FALSE)
-                        lapply(y, function(x) {
-                          x <- x[!is.na(x)]
-                          expanded <-
-                            paste0(unlist(strsplit(x[grepl("\\*", x)], "\\*")),
-                                   "")
-                          notExpanded <-
-                            paste0(x[!grepl("\\*", x)], "")
-                          if (sum(notExpanded %in% expanded) == 0)
-                            return(x)
-                          else
-                            return(NULL)
-                        })
-                      })
+    combs <- pblapply(cl = clust, minvar:maxvar, function(x) {
+      lapply(combn(independent, x, simplify = FALSE), dropDuplicates)
+      })
   } else {
-    combs <- pblapply(minvar:maxvar,
-                      function(x) {
-                        y <- combn(independent, x, simplify = FALSE)
-                        lapply(y, function(x) {
-                          x <- x[!is.na(x)]
-                          expanded <-
-                            paste0(unlist(strsplit(x[grepl("\\*", x)], "\\*")),
-                                   "")
-                          notExpanded <-
-                            paste0(x[!grepl("\\*", x)], "")
-                          if (sum(notExpanded %in% expanded) == 0)
-                            return(x)
-                          else
-                            return(NULL)
-                        })
-                      })
+    combs <- pblapply(minvar:maxvar, function(x) {
+      lapply(combn(independent, x, simplify = FALSE), dropDuplicates)
+      })
   }
   combs <- unlist(combs, recursive = FALSE)
-  print(paste("Creating", length(combs), "formulas. Please be patient, this may take a while."))
+  print(paste("Creating",
+              length(combs),
+              "formulas. Please be patient, this may take a while."))
 
   # create a base data.frame that has all independent variables
   # `*` in interaction terms is converted to a `.`
@@ -176,42 +157,13 @@ regsearch <- function(data,
   print("Creating regressions...")
   if (multi) {
     clusterExport(clust, c("combs"), envir = environment())
-    forms <- pblapply(cl = clust, combs, function(x) {
-      if (is.null(x))
-        return(NULL)
-      paste(paste(dependent, "~"),
-            paste("+", x[!is.na(x)], collapse = " "),
-            collapse = " ")
-    })
+    forms <-
+      pblapply(cl = clust, combs, createRegressions, dependent = dependent)
   } else {
-    forms <- pblapply(combs, function(x) {
-      if (is.null(x))
-        return(NULL)
-      paste(paste(dependent, "~"),
-            paste("+", x[!is.na(x)], collapse = " "),
-            collapse = " ")
-    })
+    forms <- pblapply(combs, createRegressions, dependent = dependent)
   }
   forms <- unlist(forms)
   print(paste("Running", length(forms), "regressions. Please be patient, this may take a while."))
-
-  # takes a character vector or formula and runs the requested regression
-  # returns a single row data.table representing the results of that regression
-  summFunc <- function(x) {
-    options(warn = 0)
-    summ <- summary(glm(formula = as.formula(x), data = data, family = family))
-    # warn <- tryCatch({ names(last.warning) },
-    #                  warning = function(e) {""},
-    #                  error = function(e) {""})
-    coefs <- t(summ$coefficients[,4])
-    names(coefs) <- gsub(":", ".", names(coefs))
-    summ <- data.frame("aic" = summ$aic,
-                       "rSquare" = round(1 - (summ$deviance / summ$null.deviance), 5),
-                       # "warn" = warn,
-                       "warn" = NA,
-                       coefs)
-    rbindlist(list(reg, summ), fill = TRUE)
-  }
 
   # runs the list of regressions and builds a data.table with the results of
   #   every regression
@@ -219,9 +171,13 @@ regsearch <- function(data,
   if(multi) {
     clusterEvalQ(clust, library(data.table))
     clusterExport(clust, c("forms", "family", "data"), envir = environment())
-    regs <- rbindlist(pblapply(cl = clust, forms, summFunc), fill = TRUE)
+    regs <- rbindlist(pblapply(cl = clust, forms, summariseReg,
+                               data = data, family = family, reg = reg, ...),
+                      fill = TRUE)
   } else {
-    regs <- rbindlist(pblapply(forms, summFunc), fill = TRUE)
+    regs <- rbindlist(pblapply(forms, summariseReg,
+                               data = data, family = family, reg = reg, ...),
+                      fill = TRUE)
   }
 
   # attaches the formulas to the data.table
@@ -240,14 +196,49 @@ regsearch <- function(data,
   # because regressions themselves are not stored, they are run again
   if (topN > 0) {
     sapply(regs[1:topN, formula], function(x) {
-      print(summary(glm(
-        formula = as.formula(x),
-        data = data,
-        family = family
-      )))
+      print(summary(glm(formula = as.formula(x), data = data, family = family)))
     })
   }
 
   # return the final data.table
   return(regs)
+}
+
+# takes a character vector or formula and runs the requested regression
+# returns a single row data.table representing the results of that regression
+summariseReg <- function(x, data, family, reg, ...) {
+  options(warn = 0)
+  summ <- summary(glm(formula = as.formula(x), data = data, family = family, ...))
+  # warn <- tryCatch({ names(last.warning) },
+  #                  warning = function(e) {""},
+  #                  error = function(e) {""})
+  coefs <- t(summ$coefficients[,4])
+  names(coefs) <- gsub(":", ".", names(coefs))
+  summ <- data.frame("aic" = summ$aic,
+                     "rSquare" = round(1 - (summ$deviance / summ$null.deviance), 5),
+                     # "warn" = warn,
+                     "warn" = NA,
+                     coefs)
+  return(rbindlist(list(reg, summ), fill = TRUE))
+}
+
+# compares expanded (interactions) to unexpanded single variables
+# returns the input if no unexpanded variables are in the expanded variables
+dropDuplicates <- function(x) {
+  x <- x[!is.na(x)]
+  expanded <- paste0(unlist(strsplit(x[grepl("\\*", x)], "\\*")), "")
+  notExpanded <- paste0(x[!grepl("\\*", x)], "")
+  if (sum(notExpanded %in% expanded) == 0)
+    return(x)
+  else
+    return(NULL)
+}
+
+# pastes variables into a usable format for regressions
+createRegressions <- function(x, dependent) {
+  if (is.null(x))
+    return(NULL)
+  return(paste(paste(dependent, "~"),
+               paste("+", x[!is.na(x)], collapse = " "),
+               collapse = " "))
 }
